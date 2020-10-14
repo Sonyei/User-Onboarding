93e556d0b9e8cffa94ca9b972cec8f4de254a672
Sonyei <esonyei@gmail.com>
2020-10-14 16:06:32 -0400

    initial project steps up

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios'
+import * as yup from 'yup'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
